322ec65b917bbc4a15cb1fef78a69da6c22c11f5
tsfnis <tsfnis@gmail.com>
2018-01-15 19:17:54 -0500

    final interctive circles

--- session2/sketch.js
+++ session2/sketch.js
@@ -1,14 +1,18 @@
 
 function setup() {
   createCanvas(700, 400);
+
 }
 
 function draw() {
   background(240, 200, 200);
+
+  if (mouseIsPressed){
+    background(mouseY, mouseX, 200);
+  }
   // this is my cool loop for circles that move
-  for(var i=0; i < 10; i++) {
-    fill(random(255),random(255),random(255));
-    ellipse(mouseX/i, 200, 20, 20);
+  for(var i=0; i < 6; i++) {
+    ellipse(mouseX/i, 200, mouseY/i, mouseY/i);
   }
 
 }
